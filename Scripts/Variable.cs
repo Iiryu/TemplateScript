9ab44151c4fefabd58bacbf1d29525b108c9b472
Iiryu <76665256+Iiryu@users.noreply.github.com>
2021-09-12 05:51:22 +0900

    Create Variable.cs

---
+++ Scripts/Variable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Variable : MonoBehaviour
+{
+    public int number,number2;
+    public float num,num2
+    public bool button,button2
+    public string sentence,sentence2;
+    public List<string> list = new List<string>();
+}
